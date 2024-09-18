2a4788111aa581b5d15140ee05a58d454a4a8491
R.P.Laksitha Dineth Maduranga <dinethlaksitha@gmail.com>
2024-09-18 16:47:49 +0530

    updated

---
+++ src/pages/CreateListing.jsx
@@ -0,0 +1,15 @@
+import "../styles/CreateListing.scss"
+import Navbar from "../components/Navbar"
+
+const CreateListing = () => {
+    return (
+        <>
+            <Navbar />
+            <div>
+                <h1>Create New Listing</h1>
+            </div>
+        </>
+    )
+}
+
+export default CreateListing
